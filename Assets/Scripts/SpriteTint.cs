51ce3dcc6b14e44f52a5da42ce14d977a3715fe1
Jorge Luis <jldealba@cocoschool.mx>
2020-03-10 14:49:37 -0600

    TintEffect

---
+++ Assets/Scripts/SpriteTint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTint : MonoBehaviour
+{
+    SpriteRenderer srenderer;
+
+    private void Awake()
+    {
+        srenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+            StartCoroutine(Damage());
+    }
+    IEnumerator Damage()
+    {
+        srenderer.color = Color.white;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.red;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.white;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.red;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.white;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.red;
+        yield return new WaitForSeconds(.3f);
+        srenderer.color = Color.white;
+    }
+}
