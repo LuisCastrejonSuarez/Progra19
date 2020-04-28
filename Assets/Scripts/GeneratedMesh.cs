d375be2b2e52073d2809b260bfc350490b3880a7
Luis Monster <luis@big-monster.net>
2020-04-28 15:13:44 -0500

    Generated Cube and UV Test Texture

--- Assets/Scripts/GeneratedMesh.cs
+++ Assets/Scripts/GeneratedMesh.cs
@@ -7,29 +7,62 @@ public class GeneratedMesh : MonoBehaviour
     public Vector3[] vertex;
     public Vector2[] uvmap;
     public int[] triangles;
-
+    private MeshFilter filter;
+    private void Awake()
+    {
+        filter = gameObject.AddComponent<MeshFilter>();
+    }
     private void Start()
     {
+        CreateMesh();
+    }
+    private void Update()
+    {
+        //CreateMesh();
+    }
+    private void CreateMesh()
+    {
         // vertices
-        vertex = new Vector3[] {new Vector3(-1, 0, 1),
-                                new Vector3( 1, 0, 1),
-                                new Vector3( 1, 0,-1),
-                                new Vector3(-1, 0,-1)};
-        // uv map
-        uvmap = new Vector2[] { new Vector2(0,256),
-                                new Vector2(256,256),
-                                new Vector2(256,0),
-                                new Vector2(0,0)};
+        vertex = new Vector3[] {new Vector3(-1, -1, 1),     // vertice 0
+                                new Vector3( 1, -1, 1),     // vertice 1
+                                new Vector3( 1, -1, -1),    // vertice 2
+                                new Vector3( -1, -1, -1),   // vertice 3
+                                new Vector3(-1, 1, 1),      // vertice 4
+                                new Vector3( 1, 1, 1),      // vertice 5
+                                new Vector3( 1, 1, -1),     // vertice 6
+                                new Vector3( -1, 1, -1)};   // vertice 7
+
         // poligonos
-        triangles = new int[] { 0,1,2,
-                                0,2,3 };
+        triangles = new int[] { 4,5,6,
+                                4,6,7,
+                                0,2,1,
+                                0,3,2,
+                                7,6,2,
+                                7,2,3,
+                                4,7,3,
+                                4,3,0,
+                                6,5,1,
+                                6,1,2,
+                                5,4,0,
+                                5,0,1};
+
+        // uv map
+        uvmap = new Vector2[] { new Vector2(1,0),
+                                new Vector2(0,0),
+                                new Vector2(0,1),
+                                new Vector2(1,1),
+                                new Vector2(1,0),
+                                new Vector2(0,0),
+                                new Vector2(0,1),
+                                new Vector2(1,1),
+                               };
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertex;
         mesh.triangles = triangles;
+        mesh.RecalculateNormals();
         mesh.uv = uvmap;
 
-        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
         filter.mesh = mesh;
     }
 }
