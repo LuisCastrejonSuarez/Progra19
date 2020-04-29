68b100ec530b21b09111437e0a0b2da26d821c97
Luis Monster <luis@big-monster.net>
2020-04-29 10:43:46 -0500

    UV Map Session 01

--- Assets/Scripts/GeneratedMesh.cs
+++ Assets/Scripts/GeneratedMesh.cs
@@ -2,59 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
 public class GeneratedMesh : MonoBehaviour
 {
     public Vector3[] vertex;
     public Vector2[] uvmap;
     public int[] triangles;
     private MeshFilter filter;
-    private void Awake()
-    {
-        filter = gameObject.AddComponent<MeshFilter>();
-    }
+    
     private void Start()
     {
         CreateMesh();
     }
     private void Update()
     {
-        //CreateMesh();
+        CreateMesh();
     }
     private void CreateMesh()
     {
         // vertices
-        vertex = new Vector3[] {new Vector3(-1, -1, 1),     // vertice 0
+        vertex = new Vector3[]{ /* vertices inferiores */
+                                new Vector3(-1, -1, 1),     // vertice 0
                                 new Vector3( 1, -1, 1),     // vertice 1
                                 new Vector3( 1, -1, -1),    // vertice 2
                                 new Vector3( -1, -1, -1),   // vertice 3
+                                /* vertices superiores */
                                 new Vector3(-1, 1, 1),      // vertice 4
                                 new Vector3( 1, 1, 1),      // vertice 5
                                 new Vector3( 1, 1, -1),     // vertice 6
-                                new Vector3( -1, 1, -1)};   // vertice 7
+                                new Vector3( -1, 1, -1),    // vertice 7
+                                /* vertices frontales */
+                                new Vector3( -1, 1, -1),     // vertice 8
+                                new Vector3( 1, 1, -1),     // vertice 9
+                                new Vector3( 1, -1, -1),     // vertice 10
+                                new Vector3( -1, -1, -1),     // vertice 11
+        };   
 
         // poligonos
-        triangles = new int[] { 4,5,6,
+        triangles = new int[] { // cara superior
+                                4,5,6, 
                                 4,6,7,
+                                // cara inferior
                                 0,2,1,
                                 0,3,2,
-                                7,6,2,
-                                7,2,3,
-                                4,7,3,
-                                4,3,0,
-                                6,5,1,
-                                6,1,2,
-                                5,4,0,
-                                5,0,1};
+                                // cara frontal
+                                8,9,10,
+                                8,10,11,
+                                };
 
         // uv map
-        uvmap = new Vector2[] { new Vector2(1,0),
-                                new Vector2(0,0),
+        uvmap = new Vector2[] { /* vertices inferiores */
+                                new Vector2(0.250f,1),
+                                new Vector2(0.125f,1),
+                                new Vector2(0.125f,0.875f),
+                                new Vector2(0.250f,0.875f),
+                                /* vertices superiores */
                                 new Vector2(0,1),
-                                new Vector2(1,1),
-                                new Vector2(1,0),
-                                new Vector2(0,0),
-                                new Vector2(0,1),
-                                new Vector2(1,1),
+                                new Vector2(0.125f,1),
+                                new Vector2(0.125f,0.875f),
+                                new Vector2(0,0.875f),
+                                /* vertices frontales */
+                                new Vector2(0.125f,1),
+                                new Vector2(0.250f,1),
+                                new Vector2(0.250f,0.875f),
+                                new Vector2(0.125f,0.875f),
                                };
 
         Mesh mesh = new Mesh();
@@ -62,7 +75,7 @@ public class GeneratedMesh : MonoBehaviour
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
         mesh.uv = uvmap;
-
+        filter = gameObject.GetComponent<MeshFilter>();
         filter.mesh = mesh;
     }
 }
